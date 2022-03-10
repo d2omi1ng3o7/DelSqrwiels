72121be72a4a85c54b3f13927a47bbf0e93754d7
d2omi1ng3o7 <dawidmucha@elektronikonline.pl>
2022-03-10 18:13:14 +0100

    dsafasd

--- Content.js
+++ Content.js
@@ -1,20 +1,25 @@
-function delSidePanel(){
+function delNavigation(){
+    document.getElementById("mw-navigation").style.display = 'none';
+    document.getElementById("footer").style.display = 'none';
+    document.getElementById("mw-head-base").style.display = 'none';
+    document.getElementById("mw-page-base").style.display = 'none';
+    document.getElementById("content").style.border = '0';
+    document.getElementById("content").style.margin = '0';
     
 }
 
-function delNav(){
-    
-}
-
-function delFooter(){
-    
-}
 
 function delReference(){
     const references = document.getElementsByClassName("reference");
 
     for (let i = 0; i < references.length; i++){
         references[i].style.display = 'none';
+    }
+
+    const references2 = document.getElementsByClassName("do-not-make-smaller refsection");
+
+    for (let i = 0; i < references2.length; i++){
+        references2[i].style.display = 'none';
     }
 }
 
@@ -23,7 +28,20 @@ function delTable(){
 }
 
 function setFont(){
-    document.getElementsByTagName("body").style.fontfamily = "sans-serif";
+    const title = document.getElementsByTagName("h1")
+    
+    for (let i = 0; i < title.length; i++){
+        title[i].style.fontFamily = "sans-serif";
+    }
+
+    const title2 = document.getElementsByTagName("h2")
+    
+    for (let i = 0; i < title2.length; i++){
+        title2[i].style.fontFamily = "sans-serif";
+    }
 }
 
-document.getElementById("option-6").addEventListener("click", setFont);
+delNavigation()
+delReference()
+delTable()
+setFont()
