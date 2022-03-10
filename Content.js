7c635cdb92a89bc011248d3f6bc29e08ee2e7b1b
d2omi1ng3o7 <dawidmucha@elektronikonline.pl>
2022-03-10 17:25:59 +0100

    commit434

--- Content.js
+++ Content.js
@@ -1,5 +1,29 @@
-const references = document.getElementsByClassName("reference")
-
-for (let i = 0; i < references.length; i++){
-    references[i].style.display = 'none'
+function delSidePanel(){
+    
 }
+
+function delNav(){
+    
+}
+
+function delFooter(){
+    
+}
+
+function delReference(){
+    const references = document.getElementsByClassName("reference");
+
+    for (let i = 0; i < references.length; i++){
+        references[i].style.display = 'none';
+    }
+}
+
+function delTable(){
+    
+}
+
+function setFont(){
+    document.getElementsByTagName("body").style.fontfamily = "sans-serif";
+}
+
+document.getElementById("option-6").addEventListener("click", setFont);
